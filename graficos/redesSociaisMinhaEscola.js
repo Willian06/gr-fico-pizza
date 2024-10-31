6dd7fc5b3d1289c39abed5455bd7ca559a480b97
Willian06 <126635903+Willian06@users.noreply.github.com>
2024-10-31 13:36:50 -0300

    y

--- graficos/redesSociaisMinhaEscola.js
+++ graficos/redesSociaisMinhaEscola.js
@@ -54,7 +54,7 @@ function processarDados(dados) {
     }
 
     criarGrafico(data, layout)
-    incluirTexto(`Como no Brasil, a amostra de pessoas entrevistadas pelo <span>IBGE</span>, relatam o abandono infantil em cada região `)
+    incluirTexto(`Como no Brasil, a amostra de pessoas entrevistadas pelo <span>IBGE</span>, relatam a porcentagem do abandono infantil em cada região `)
 }
 
 redesSociaisFavoritasMinhaEscola()
